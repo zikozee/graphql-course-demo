f7b9005ab9ce2eaf4da53faf415a3c68c5cc67cb
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2024-09-14 13:20:12 +0100

    Validation closes #24

--- src/main/java/com/zee/graphqlcourse/config/GraphqlConfig.java
+++ src/main/java/com/zee/graphqlcourse/config/GraphqlConfig.java
@@ -1,6 +1,9 @@
 package com.zee.graphqlcourse.config;
 
 import graphql.scalars.ExtendedScalars;
+import graphql.validation.rules.OnValidationErrorStrategy;
+import graphql.validation.rules.ValidationRules;
+import graphql.validation.schemawiring.ValidationSchemaWiring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
@@ -17,6 +20,12 @@ public class GraphqlConfig {
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
 
+        ValidationSchemaWiring schemaWiring = new ValidationSchemaWiring(
+                ValidationRules.newValidationRules()
+                        .onValidationErrorStrategy(OnValidationErrorStrategy.RETURN_NULL)
+                        .build()
+        );
+
         return wiringBuilder -> wiringBuilder
                 .scalar(ExtendedScalars.NegativeInt)
                 .scalar(ExtendedScalars.NonNegativeInt)
@@ -29,6 +38,9 @@ public class GraphqlConfig {
                 .scalar(ExtendedScalars.DateTime)
                 .scalar(ExtendedScalars.GraphQLBigDecimal)
                 .scalar(ExtendedScalars.Currency)
+
+                .directiveWiring(schemaWiring)
+
                 .build();
     }
 }
